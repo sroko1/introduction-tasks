3615720dc31f24e75ad1ccee6556bf9a042d86d0
sroko1 <70091227+sroko1@users.noreply.github.com>
2021-12-08 10:00:29 +0100

    Update Task1_1.java

--- src/main/java/pl/sda/introductiontasks/tasks1/Task1_1.java
+++ src/main/java/pl/sda/introductiontasks/tasks1/Task1_1.java
@@ -1,13 +1,15 @@
 package pl.sda.introductiontasks.tasks1;
 
 public class Task1_1 {
-  /*
-  Create a variable named 'x' and initialize it with any integer.
-  In case 'x' is even, in the console print 'Is even', else print 'Is odd'.
-   */
-  public static void main(String[] args) {
-    /*
-    Write your code here
-   */
-  }
+  
+  static int x;
+
+    public static void main(String[] args) {
+        //condition for even
+        if (x % 2 == 0)
+            System.out.println("Even number");
+            //condition for odd
+        else
+            System.out.println("Odd number");
+    }
 }
