6c50f21728aab6be58b70f27cf513038d73ad984
sroko1 <70091227+sroko1@users.noreply.github.com>
2021-12-08 10:11:59 +0100

    Update Task2_1.java

--- src/main/java/pl/sda/introductiontasks/tasks2/Task2_1.java
+++ src/main/java/pl/sda/introductiontasks/tasks2/Task2_1.java
@@ -7,8 +7,13 @@ Calculate sum of all numbers in this array and print it in the console.
 
 public class Task2_1 {
   public static void main(String[] args) {
-   /*
-    Write your code here
-   */
-  }
+    int[] array = {1, 54, 17, 23, -2, 18, 22, 98, 2, -5};
+        // get stream of elements
+        IntStream stream = IntStream.of(array);
+        // add the elements in stream
+        int sum = stream.sum();
+        System.out.println("Sum of array elements is: " + sum);
+    }
 }
+  
+     
